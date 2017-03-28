3a83a6c62fb4f7d08da290861fccd65042163d04
Desmond Horsley <DHorsley@biscit.com.au>
2017-03-29 02:20:56 +1100

    Test inFlowState for new gameState

--- spec/gameStateSpec.js
+++ spec/gameStateSpec.js
@@ -6,3 +6,11 @@ describe("gameStateConstructor", function() {
         expect(gs.flowState).toEqual(GameState.FlowStateEnum.NOTFLOWING);
     });
 });
+
+describe("inFlowState", function() {
+    it("returns true for NotFlowing on new board", function() {
+        var gs = new GameState();
+        expect(gs.inFlowState(GameState.FlowStateEnum.NOTFLOWING)).toBeTruthy();
+    });
+});
+
